1b6427d68edd8aad55a76de65665d43f82660d98
Sanjida Akter <sanjidakter2304@gmail.com>
2023-03-28 02:54:17 +0600

    product cart added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop'
+import Product from './components/Product/Product'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +12,8 @@ function App() {
   return (
     <div className="App">
      <Header></Header>
-     
+     <Shop></Shop>
+    
     </div>
   )
 }
