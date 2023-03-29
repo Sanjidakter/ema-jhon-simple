d5a94925d1637e79784d14eed3fb71fe6ed16da6
Sanjida Akter <sanjidakter2304@gmail.com>
2023-03-29 12:43:35 +0600

    retrive product from stored cart or local storage and set it ti cart state

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -12,10 +13,39 @@ const Shop = () => {
         .then(res => res.json())
         .then(data => setProducts(data))
     },[]);
+    useEffect(() =>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step 1: get id of the addedPRoduct
+        for(const id in storedCart){
+            // step 2 get the product by using id 
+            const addedProduct = products.find(product => product.id === id)
+            // step 3:get quantity of the product
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the added product to the saved cart
+                savedCart.push(addedProduct)
+            }
+          console.log('added product',addedProduct)
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    },[products])
+
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        // step 1 : get id of the addedProduct
+        for(const id in storedCart){
+            // get product from products state by using id 
+            const addedProduct = products.find(product => product.id === id)
+        }
+    })
     // state immutable set diye change kora lagbe 
     const handleAddToCart = (product) =>{
        const newCart = [...cart,product];
        setCart(newCart);
+       addToDb(product.id)
     }
     return (
         <div className='shop-container'>
