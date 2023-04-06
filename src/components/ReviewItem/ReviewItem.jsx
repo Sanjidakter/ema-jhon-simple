aa1af84a47eccffedfd5024ad8d9535020ba47b9
Sanjida Akter <sanjidakter2304@gmail.com>
2023-04-06 14:26:42 +0600

    delete button functioning and deleting products fro fakedb

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './ReviewItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-const ReviewItem = ({product}) => {
+const ReviewItem = ({product,handleRemoveFromCart}) => {
+    const {id,name,quantity,price,img } = product;
     return (
-        <div>
-            <h2>Review Item</h2>
+        <div className='review-item'>
+            <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price} </span></p>
+                <p>Order Quantity: <span className='orange-text'>${price} </span></p>
+            </div>
+            <button onClick={()=>handleRemoveFromCart(id)} className='btn-delete'> <FontAwesomeIcon className='dlt-icon' icon={faTrashAlt} /></button>
         </div>
     );
 };
